73f44792817f3330f54ec925b4feef4cef4ecc8d
user1 <user1@mac007.local>
2020-04-02 22:09:40 +0800

    WS5 finished

---
+++ ios_ws/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios_ws
+//
+//  Created by user1 on 2/4/2020.
+//  Copyright © 2020 COMP7506. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
